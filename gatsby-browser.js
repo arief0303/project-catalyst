eeb86350bd85ca0cfe32d1e90f35765ed2aeea6e
Arief R.Syauqie <arief0303@gmail.com>
2023-07-17 23:37:40 +0700

    init

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
+import * as React from "react"
+import { GlobalCanvas } from '@14islands/r3f-scroll-rig'
 import "./src/styles/global.css"
+
+export const wrapRootElement = ({ element }) => (
+  <>
+    <GlobalCanvas />
+    {element}
+  </>
+)
