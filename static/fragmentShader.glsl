cc22c82f6e598c9c8a4c43163dc1ec01665306e6
arief0303 <arief0303@gmail.com>
2023-09-20 22:29:04 +0700

    halo shader

--- static/fragmentShader.glsl
+++ static/fragmentShader.glsl
@@ -2,121 +2,120 @@ uniform float iTime;
 uniform vec2 iResolution;
 varying vec2 vUv;
 uniform sampler2D iChannel0;
-// by srtuss, 2013
-// https://www.shadertoy.com/view/4sl3Dr#
-// rotate position around axis
-vec2 rotate(vec2 p, float a) {
-    return vec2(p.x * cos(a) - p.y * sin(a), p.x * sin(a) + p.y * cos(a));
+
+// noise from https://www.shadertoy.com/view/4sc3z2
+vec3 hash33(vec3 p3)
+{
+	p3 = fract(p3 * vec3(.1031,.11369,.13787));
+    p3 += dot(p3, p3.yxz+19.19);
+    return -1.0 + 2.0 * fract(vec3(p3.x+p3.y, p3.x+p3.z, p3.y+p3.z)*p3.zyx);
 }
 
-// 1D random numbers
-float rand(float n) {
-    return fract(sin(n) * 43758.5453123);
+float snoise3(vec3 p)
+{
+    const float K1 = 0.333333333;
+    const float K2 = 0.166666667;
+    
+    vec3 i = floor(p + (p.x + p.y + p.z) * K1);
+    vec3 d0 = p - (i - (i.x + i.y + i.z) * K2);
+    
+    vec3 e = step(vec3(0.0), d0 - d0.yzx);
+	vec3 i1 = e * (1.0 - e.zxy);
+	vec3 i2 = 1.0 - e.zxy * (1.0 - e);
+    
+    vec3 d1 = d0 - (i1 - K2);
+    vec3 d2 = d0 - (i2 - K1);
+    vec3 d3 = d0 - 0.5;
+    
+    vec4 h = max(0.6 - vec4(dot(d0, d0), dot(d1, d1), dot(d2, d2), dot(d3, d3)), 0.0);
+    vec4 n = h * h * h * h * vec4(dot(d0, hash33(i)), dot(d1, hash33(i + i1)), dot(d2, hash33(i + i2)), dot(d3, hash33(i + 1.0)));
+    
+    return dot(vec4(31.316), n);
 }
 
-// 2D random numbers
-vec2 rand2(in vec2 p) {
-    return fract(vec2(sin(p.x * 591.32 + p.y * 154.077), cos(p.x * 391.32 + p.y * 49.077)));
-}
-
-// 1D noise
-float noise1(float p) {
-    float fl = floor(p);
-    float fc = fract(p);
-    return mix(rand(fl), rand(fl + 1.0), fc);
-}
-
-// voronoi distance noise, based on iq's articles
-float voronoi(in vec2 x) {
-    vec2 p = floor(x);
-    vec2 f = fract(x);
-
-    vec2 res = vec2(8.0);
-    for(int j = -1; j <= 1; j++) {
-        for(int i = -1; i <= 1; i++) {
-            vec2 b = vec2(i, j);
-            vec2 r = vec2(b) - f + rand2(p + b);
-
-   // chebyshev distance, one of many ways to do this
-            float d = max(abs(r.x), abs(r.y));
-
-            if(d < res.x) {
-                res.y = res.x;
-                res.x = d;
-            } else if(d < res.y) {
-                res.y = d;
-            }
-        }
-    }
-    return res.y - res.x;
-}
-
-void main() {
-    vec2 fragCoord = vUv * iResolution;
-
-    float flicker = noise1(iTime * 2.0) * 0.8 + 0.4;
-
-    vec2 uv = fragCoord.xy / iResolution.xy;
-    uv = (uv - 0.5) * 2.0;
-    vec2 suv = uv;
-    uv.x *= iResolution.x / iResolution.y;
-
-    float v = 0.0;
-
- // that looks highly interesting:
- //v = 1.0 - length(uv) * 1.3;
-
- // a bit of camera movement
-    uv *= 0.6 + sin(iTime * 0.1) * 0.4;
-    uv = rotate(uv, sin(iTime * 0.3) * 1.0);
-    uv += iTime * 0.4;
-
- // add some noise octaves
-    float a = 0.6, f = 1.0;
-
-    for(int i = 0; i < 3; i++) // 4 octaves also look nice, its getting a bit slow though
+vec4 extractAlpha(vec3 colorIn)
+{
+    vec4 colorOut;
+    float maxValue = min(max(max(colorIn.r, colorIn.g), colorIn.b), 1.0);
+    if (maxValue > 1e-5)
     {
-        float v1 = voronoi(uv * f + 5.0);
-        float v2 = 0.0;
-
-  // make the moving electrons-effect for higher octaves
-        if(i > 0) {
-   // of course everything based on voronoi
-            v2 = voronoi(uv * f * 0.5 + 50.0 + iTime);
-
-            float va = 0.0, vb = 0.0;
-            va = 1.0 - smoothstep(0.0, 0.1, v1);
-            vb = 1.0 - smoothstep(0.0, 0.08, v2);
-            v += a * pow(va * (0.5 + vb), 2.0);
-        }
-
-  // make sharp edges
-        v1 = 1.0 - smoothstep(0.0, 0.3, v1);
-
-  // noise is used as intensity map
-        v2 = a * (noise1(v1 * 5.5 + 0.1));
-
-  // octave 0's intensity changes a bit
-        if(i == 0)
-            v += v2 * flicker;
-        else
-            v += v2;
-
-        f *= 3.0;
-        a *= 0.7;
+        colorOut.rgb = colorIn.rgb * (1.0 / maxValue);
+        colorOut.a = maxValue;
     }
+    else
+    {
+        colorOut = vec4(0.0);
+    }
+    return colorOut;
+}
 
- // slight vignetting
-    v *= exp(-0.6 * length(suv)) * 1.2;
+#define BG_COLOR (vec3(sin(iTime)*0.5+0.5) * 0.0 + vec3(0.0))
+#define time iTime
+const vec3 color1 = vec3(0.611765, 0.262745, 0.996078);
+const vec3 color2 = vec3(0.298039, 0.760784, 0.913725);
+const vec3 color3 = vec3(0.062745, 0.078431, 0.600000);
+const float innerRadius = 0.6;
+const float noiseScale = 0.65;
 
- // use texture channel0 for color? why not.
-    vec3 cexp = texture(iChannel0, uv * 0.001).xyz * 3.0 + texture(iChannel0, uv * 0.01).xyz;//vec3(1.0, 2.0, 4.0);
-    cexp *= 1.4;
+float light1(float intensity, float attenuation, float dist)
+{
+    return intensity / (1.0 + dist * attenuation);
+}
+float light2(float intensity, float attenuation, float dist)
+{
+    return intensity / (1.0 + dist * dist * attenuation);
+}
 
- // old blueish color set
- //vec3 cexp = vec3(6.0, 4.0, 2.0);
+void draw( out vec4 _FragColor, in vec2 vUv )
+{
+    vec2 uv = vUv;
+    float ang = atan(uv.y, uv.x);
+    float len = length(uv);
+    float v0, v1, v2, v3, cl;
+    float r0, d0, n0;
+    float r, d;
+    
+    // ring
+    n0 = snoise3( vec3(uv * noiseScale, time * 0.5) ) * 0.5 + 0.5;
+    r0 = mix(mix(innerRadius, 1.0, 0.4), mix(innerRadius, 1.0, 0.6), n0);
+    d0 = distance(uv, r0 / len * uv);
+    v0 = light1(1.0, 10.0, d0);
+    v0 *= smoothstep(r0 * 1.05, r0, len);
+    cl = cos(ang + time * 2.0) * 0.5 + 0.5;
+    
+    // high light
+    float a = time * -1.0;
+    vec2 pos = vec2(cos(a), sin(a)) * r0;
+    d = distance(uv, pos);
+    v1 = light2(1.5, 5.0, d);
+    v1 *= light1(1.0, 50.0 , d0);
+    
+    // back decay
+    v2 = smoothstep(1.0, mix(innerRadius, 1.0, n0 * 0.5), len);
+    
+    // hole
+    v3 = smoothstep(innerRadius, mix(innerRadius, 1.0, 0.5), len);
+    
+    // color
+    vec3 c = mix(color1, color2, cl);
+    vec3 col = mix(color1, color2, cl);
+    col = mix(color3, col, v0);
+    col = (col + v1) * v2 * v3;
+    col.rgb = clamp(col.rgb, 0.0, 1.0);
+    
+    //gl_FragColor = extractAlpha(col);
+    _FragColor = extractAlpha(col);
+}
 
-    vec3 col = vec3(pow(v, cexp.x), pow(v, cexp.y), pow(v, cexp.z)) * 2.0;
+void main()
+{
+    vec2 fragCoord = vUv * iResolution;
+    vec2 uv = (fragCoord*2.-iResolution.xy)/iResolution.y;
+    
+    vec4 col;
+    draw(col, uv);
 
-    gl_FragColor = vec4(col, 1.0);
+    vec3 bg = BG_COLOR;
+
+    gl_FragColor.rgb = mix(bg, col.rgb, col.a); //normal blend
 }
